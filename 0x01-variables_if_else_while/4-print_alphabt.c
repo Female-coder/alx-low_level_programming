d3fe98dec8679386791f9e6068235e49d1fad118
Female-coder <magadanineo@gmail.com>
2023-09-11 13:28:31 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main-main block
+ * describe random number.
+ *
+ * Return: 0
+ */
+
+int main(void)
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z';alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q'
+				putchar(alpha)
+	}
+	putchar('\n);
+	return(0);	
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main-main block
+ * describe random number.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{	
+	int num;
+
+	
