d74fc2b4e688a863548545153978c173f460523e
Female-coder <magadanineo@gmail.com>
2023-09-11 15:53:47 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
 /**
-* main - Prints a random number whether positive,negative,or zero.
-*
-* Return: Always 0.
-*/
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	m = n % 10;
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+	else if (m < 6 && m != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	}
 	else
-		printf("%d is zero\n", n);
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	}
 	return (0);
 }
-
-
