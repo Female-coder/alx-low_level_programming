4b6957ac7793334d13a2b0798d45ed95e6345bca
Female-coder <magadanineo@gmail.com>
2023-09-11 14:55:11 -0700

    ./1-last_digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main-main block/entry point
  * description:print whether random number generated is + or -.
  *
- * return: 0
+ * return: 0 (success)
  */
 
 int main(void)
@@ -16,10 +16,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	if (n == 0)
+	}
+	else (n == 0)
+	{	
 		printf("%d is zero\n", n);
-	if (n < 0)
+	}	
+	else
+	{
 		printf("%d is negative\n", n);
+	}	
 	return (0);
 }
