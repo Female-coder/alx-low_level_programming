dee563e3c67bc2841a8f03ec796e346ea1b85da2
Female-coder <magadanineo@gmail.com>
2023-09-12 15:11:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+char chara;
+for (chara = 'a'; chara <= 'z'; chara++)
+_putchar(chara)
+putchar('\n')
+}
