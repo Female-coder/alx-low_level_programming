cfefc6f31f3c2d1a1609c78a67e20a398ea2f712
Female-coder <magadanineo@gmail.com>
2023-09-15 18:51:16 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function that checks for a digit 
+ * @a: a -variable
+ * b variable
+ * Return: Always 0
+ */
+int mul (int a, int b)
+{
+	int mul;
+
+
+	mul =a * b;
+	return (mul);
+}
