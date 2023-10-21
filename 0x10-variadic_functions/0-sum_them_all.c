720065dd299de589aae97c4567b781c5bce86704
Female-coder <magadanineo@gmail.com>
2023-10-20 18:12:07 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,21 @@
 #include "variadic_functions.h"
 
 /**
- * sum_of all - total of all paramaters of function
- * @n: number of argument
+ * sum_them_all - sum all paramaters of function
+ * @n: number of arguments to sum
  *
  * Return: sum of all characters provided
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	/* declaring the sum variable */
+	/* declare sum variable */
 	unsigned int sum = 0;
 	unsigned int i;
 
-	/* declaing the e va_list data_type */
+	/* declare va_list data_type */
 	va_list ptr_n;
 
-	/* initializing the variadic function args */
+	/* initialize variadic function args */
 	va_start(ptr_n, n);
 	/* supply sum args and iterate elements of func */
 	for (i = 0; i < n; i++)
@@ -31,4 +31,4 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(ptr_n); /* end function traversal */
 	return (sum);
-}
+}}
